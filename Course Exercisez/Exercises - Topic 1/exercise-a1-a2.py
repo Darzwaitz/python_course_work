dabad2aa5e646beed681f6ef595f6e7d0374f6ed
darzwaitz <darzwaitz@yahoo.com>
2023-03-08 23:47:26 +0000

    Course exercisez - Topic 1 - a1

---
+++ Course Exercisez/Exercises - Topic 1/exercise-a1-a2.py
@@ -0,0 +1,7 @@
+print('Hello' + ' ' + 'world')
+
+greeting = 'Hello'
+
+print(greeting + ' world')
+
+print('Hello','World')
---
+++ test.py
@@ -0,0 +1,6 @@
+# comment in py
+
+print('Darz')
+print('Whatz your name?')
+myName = input()
+print('Your name iz '+ myName + ' and length is ' + str(len(myName)))
