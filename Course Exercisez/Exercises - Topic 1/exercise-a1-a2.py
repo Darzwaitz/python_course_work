c3287d2eb868663a876e6c4adb9781d9e1fa1957
darzwaitz <darzwaitz@yahoo.com>
2023-03-08 23:56:48 +0000

    Course exercisez - Topic 1 - a2

--- Course Exercisez/Exercises - Topic 1/exercise-a1-a2.py
+++ Course Exercisez/Exercises - Topic 1/exercise-a1-a2.py
@@ -5,3 +5,24 @@ greeting = 'Hello'
 print(greeting + ' world')
 
 print('Hello','World')
+
+# Exercise 2
+# You are creating an Electronic Medical Records application and you are currently working on
+# the personal details portion of the app.
+# You need to create the following variables. Please input data that you feel is appropriate for
+# these variables. e.g. first_name = “Geoff” and so on..
+
+first_name = 'Darz'
+last_name = 'K'
+age = 88
+gender = 'male'
+weight = 12.0
+
+bmi = 32.1
+diagnoses = ['list item 1', 'list item 2']
+medications = ['list item 1', 'list item 2']
+next_of_kin = "Big T"
+has_medical_card = False
+contact_number = '5555-555-55'
+
+print(first_name, last_name, age, gender, weight)
